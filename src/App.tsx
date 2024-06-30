695e0e886a08ad60b8798eaaf5a3c500b7834dce
Carlos Terra <carlos@Carlos-Mac.local>
2024-06-29 23:02:01 -0300

    add images and tailwind css config

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,25 @@ import CharacterList from './components/CharacterList';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { useState } from 'react';
+import logo from './assets/marvel-logo.svg';
 
 function App() {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <CharacterList />
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <>
+      <div className="flex justify-center">
+        <img
+          className="w-64 md:w-80 pt-4 pb-10 transition-all duration-500"
+          src={logo}
+          alt="Marvel"
+        />
+      </div>
+      <QueryClientProvider client={queryClient}>
+        <CharacterList />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </>
   );
 }
 
